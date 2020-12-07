91e38c4eb0b585f608a48da6ddc5da1707378c35
Chuvetharani <68391363+Chuvetharani@users.noreply.github.com>
2020-12-07 20:43:39 +0530

    changes

--- src/components/App.js
+++ src/components/App.js
@@ -1,16 +1,27 @@
-import React, {Component, useState} from "react";
-import '../styles/App.css';
+import React, { Component, useState } from "react";
+import { Route, Switch, useLocation, Link } from "react-router-dom";
+import "../styles/App.css";
+import Home from "./Home";
+import About from "./About";
+import LocationDisplay from "./locationDisplay";
 
-class App extends Component {
-    render() {
-
-        return(
-            <div id="main">
-               {/* Do not remove the main div */}
-            </div>
-        )
-    }
+function App()
+ {
+const NoMatch = () => {
+  return <h1>No Match</h1>;
 }
-
+    return (
+      <div id="main">
+        <Link to="/">Home</Link>
+        <Link to="/about">About</Link>
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route exact path="/about" component={About} />
+          <Route component={NoMatch} />
+        </Switch>
+        <LocationDisplay />
+      </div>
+    );
+  }
 
 export default App;
